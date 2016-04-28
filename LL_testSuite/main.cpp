e71668f455cfb65d0c886bde4abc2539059eb42b
Luke Dercher <luke.dercher@gmail.com>
2016-04-28 12:14:40 -0500

    Added files via upload

---
+++ LL_testSuite/main.cpp
@@ -0,0 +1,16 @@
+/**
+*	@file : main.cpp
+*	@author :  Luke Derhcer
+*	@date : 4/25/16
+*/
+#include<iostream>
+#include"Node.h"
+#include"LinkedList.h"
+#include"testSuite.h"
+using namespace std;
+
+int main()
+{
+	testSuite mytest;
+	mytest.run();
+}
---
+++ LL_testSuite/testSuite.h
@@ -0,0 +1,32 @@
+/**
+*	@file : TestSuite.h
+*	@author :  Luke Dercher
+*	@date : 4/25/16
+*/
+#ifndef TESTSUITE_H
+#define TESTSUITE_H
+
+#include <iostream>
+#include <vector>
+#include "LinkedList.h"
+#include "Node.h"
+
+class testSuite
+{
+	public:
+
+		void run();
+		void testOne();
+		void testTwo();
+		void testThree();
+		void testFour();
+		void testFive();
+		void testSix();
+		void testSeven();
+		void testEight();
+		void testNine();
+		void testTen();
+		void testEleven();
+};
+
+#endif
